c89727276dcf6ca59e42ea9cfde8cc9da2570d2c
huangxueliang <huangxueliang@lbdfun.com>
2021-01-31 14:25:48 +0800

    feat: form基本功能

---
+++ src/components/AlFormItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <slot></slot>
+    <div v-if="error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ['form'],
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    error: {
+      type: String,
+      default: ''
+    }
+  },
+  created() {
+    console.log(this.form, '----')
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <al-form :model="userInfo" :rules="rules">
+      <al-form-item label="用户名">
+        <al-input v-model="userInfo.userName" placeholder="请输入用户名"></al-input>
+      </al-form-item>
+      <al-form-item label="密码">
+        <al-input v-model="userInfo.userPwd" placeholder="请输入密码"></al-input>
+      </al-form-item>
+    </al-form>
+  </div>
+</template>
+
+<script>
+import AlInput from '@/components/AlInput'
+import AlFormItem from '@/components/AlFormItem'
+import AlForm from '@/components/AlForm'
+
+export default {
+  components: {
+    AlInput,
+    AlFormItem,
+    AlForm
+  },
+  data() {
+    return {
+      userInfo: {
+        userName: '',
+        userPwd: ''
+      },
+      rules: {
+        userName: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        userPwd: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
